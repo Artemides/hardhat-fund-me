cc6f85cdb33ea1d549a7ab2766b44ba50f5974a4
Artemides <edmpulasky@gmail.com>
2023-05-22 18:39:24 -0500

    complete setup

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,12 +4,18 @@ pragma solidity 0.8.18;
 
 import "./ConverterCurrency.sol";
 
+/**
+ *@title Thid is a contract for croud founding
+ *@author edmund
+ *@notice this contract
+ */
+
 contract FundMe {
     using ConvertCurrency for uint256;
-
     address payable public immutable i_owner;
     uint256 public constant MIN_USD = 50 * 1e18;
     AggregatorV3Interface agregator;
+
     enum State {
         Withdrawn,
         Active
@@ -24,22 +30,30 @@ contract FundMe {
 
     mapping(address => FundObj) foundsByFounder;
 
-    constructor(address agregatorAddress) {
-        i_owner = payable(msg.sender);
-        agregator = AggregatorV3Interface(agregatorAddress);
-    }
+    event AccountFunder();
+    event FoundsWithdrawn();
 
     error NotEnoughDonation();
     error NotOwner();
     error WithdrawFailed();
-    event AccountFunder();
-    event FoundsWithdrawn();
 
     modifier onlyOwner() {
         if (msg.sender != i_owner) revert NotOwner();
         _;
     }
 
+    /**
+     * @dev use testnet address or get an address from local blockchains
+     * @param agregatorAddress:this is the address which contains the price from eth to usd
+     */
+    constructor(address agregatorAddress) {
+        i_owner = payable(msg.sender);
+        agregator = AggregatorV3Interface(agregatorAddress);
+    }
+
+    /**
+     * @notice the fund function is public for those who want to fund or donate a minimun usd as eth
+     */
     function fund() public payable {
         uint256 foundUSD = msg.value.getConvertionRate(agregator);
         if (foundUSD < MIN_USD) revert NotEnoughDonation();
@@ -50,6 +64,10 @@ contract FundMe {
         foundsByFounder[msg.sender].state = State.Active;
     }
 
+    /**
+     * @notice this function does not restore the ones that have founded this contract, they are registered forever
+     * @dev this function should be only called by the contract's owner
+     */
     function withdraw() public onlyOwner {
         uint256 ammount;
         for (
@@ -70,6 +88,10 @@ contract FundMe {
         emit FoundsWithdrawn();
     }
 
+    /**
+     * @notice find the founder for the purpuse of adding him again or not into the founders list
+     * @param _founder: if the founder exist
+     */
     function founderExists(address _founder) internal view returns (bool) {
         bool exists = false;
         for (
