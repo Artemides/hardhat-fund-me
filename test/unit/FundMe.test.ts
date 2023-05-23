46a6bee11b9f37bd50020c5159f5771495661e42
Artemides <edmpulasky@gmail.com>
2023-05-22 21:37:40 -0500

    test found me

--- test/unit/FundMe.test.ts
+++ test/unit/FundMe.test.ts
@@ -1,13 +1,15 @@
 import { Contract } from "ethers";
 import { deployments, ethers, getNamedAccounts } from "hardhat";
 import { FundMe, MockV3Aggregator } from "../../typechain-types";
-import { assert } from "chai";
+import { assert, expect } from "chai";
 describe("Fundme test", async () => {
   let fundMe: FundMe;
   let deployer: string;
+  let deployerAddress: string;
   let MockV3Aggregator: MockV3Aggregator;
   beforeEach(async () => {
     deployer = (await getNamedAccounts()).deployer;
+    deployerAddress = (await ethers.getSigners())[0].address;
     await deployments.fixture("all");
     fundMe = await ethers.getContract("FundMe", deployer);
     MockV3Aggregator = await ethers.getContract("MockV3Aggregator", deployer);
@@ -17,6 +19,18 @@ describe("Fundme test", async () => {
     it("sets correctly the pricefeed address", async () => {
       const priceFeedAddress = await fundMe.agregator();
       assert.equal(priceFeedAddress, MockV3Aggregator.address);
+    });
+  });
+  describe("fundMe", async () => {
+    it("fails when not enough eth is funded", async () => {
+      await expect(fundMe.fund()).to.be.revertedWithCustomError(
+        fundMe,
+        "NotEnoughDonation"
+      );
+    });
+
+    it("adds the founder into the funders list if he funds only the first time", async () => {
+      await fundMe.fund({ value: ethers.utils.parseEther("1") });
     });
   });
 });
